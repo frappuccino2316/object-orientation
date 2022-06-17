1b720cd37cea20f9cbff0538758eac6d82a8f9d1
frappuccino2316 <frappuccino2316@gmail.com>
2022-06-17 09:58:53 +0900

    ストラテジーパターンのspecを作成

---
+++ forth/business_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require_relative 'business'
+
+describe Business do
+  let(:business) { Business.new }
+
+  example '社員は起立する' do
+    expect(business.stand_up).to eq '社員は起立する'
+  end
+end
+
+describe ResponsibleBusiness do
+  let(:business) { ResponsibleBusiness.new }
+
+  example '担当は起立する' do
+    expect(business.stand_up).to eq '担当は起立する'
+  end
+end
+
+describe Chief do
+  let(:business) { Chief.new }
+
+  example '主任は起立する' do
+    expect(business.stand_up).to eq '主任は起立する'
+  end
+end
