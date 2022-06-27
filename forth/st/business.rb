c3491fd063cb82c99360f4d99677e11c125ed0a9
frappuccino2316 <frappuccino2316@gmail.com>
2022-06-27 14:10:12 +0900

    strategy pattern

--- forth/st/business.rb
+++ forth/st/business.rb
@@ -1,15 +1,20 @@
+# frozen_string_literal: true
+
+# 業務クラス
 class Business
   def standup
     '社員は起立する'
   end
 end
 
+# 担当者の業務クラス
 class ResponsibleBusiness < Business
   def standup
     '担当は起立する'
   end
 end
 
+# 主任の業務クラス
 class ChiefBusiness < Business
   def standup
     '主任は起立する'
--- forth/st/business_spec.rb
+++ forth/st/business_spec.rb
@@ -6,7 +6,7 @@ describe Business do
   let(:business) { Business.new }
 
   example '社員は起立する' do
-    expect(business.stand_up).to eq '社員は起立する'
+    expect(business.standup).to eq '社員は起立する'
   end
 end
 
@@ -14,7 +14,7 @@ describe ResponsibleBusiness do
   let(:business) { ResponsibleBusiness.new }
 
   example '担当は起立する' do
-    expect(business.stand_up).to eq '担当は起立する'
+    expect(business.standup).to eq '担当は起立する'
   end
 end
 
@@ -22,6 +22,6 @@ describe ChiefBusiness do
   let(:business) { ChiefBusiness.new }
 
   example '主任は起立する' do
-    expect(business.stand_up).to eq '主任は起立する'
+    expect(business.standup).to eq '主任は起立する'
   end
 end
